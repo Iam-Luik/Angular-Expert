24afb614147dbf1ca74a8f0e167941c077e34be6
iam-luik <castro.luik@hotmail.com>
2022-01-03 05:59:00 -0300

    components, models, decorators, services

--- MinhaAppAngular/src/app/app.module.ts
+++ MinhaAppAngular/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NomeComponentComponent } from './pasta/nome-component/nome-component.component';
+import { HomeComponent } from './home/home.component';
+import { ServicoService } from './servico.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NomeComponentComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [ServicoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
